7e1d6379f1c3f80433ebe643c613a4e8d7e4fbb4
Cookie <hieu180230@gmail.com>
2025-02-21 23:01:38 +0700

    route and audit fix

--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render('index', {title: "Home Page"});
+    res.render('index.ejs', {title: "Home Page"});
 });
 
 router.get("/about", (req, res) => {
-    res.render('about', {title: "Home Page"});
+    res.render('about.ejs', {title: "About Page"});
 });
 
 router.get("/projects", (req, res) => {
-    res.render('projects', {title: "Home Page"});
+    res.render('projects.ejs', {title: "Projects Page"});
 });
 
 router.get("/skills", (req, res) => {
-    res.render('skills', {title: "Home Page"});
+    res.render('skills.ejs', {title: "Skills Page"});
 });
 
 router.get("/contact", (req, res) => {
-    res.render('contact', {title: "Home Page"});
+    res.render('contact.ejs', {title: "Contact Page"});
 });
 
 router.get("/secret", (req, res) => {
-    res.render('secret', {title: "Home Page"});
+    res.render('secret.ejs', {title: "Secret Page"});
 });
 module.exports = router;
