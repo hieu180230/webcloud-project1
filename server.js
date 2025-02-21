cee85fbc4dd4785de86a4dca59cf190e7893936f
Cookie <hieu180230@gmail.com>
2025-02-21 13:35:38 +0700

    add blob

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const app = express();
 const body_parser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-global.ROOT_DIR = __dirname;
+const ROOT_DIR = __dirname;
 
-mongoose.connect('mongodb+srv://nnhieu22:'+ process.env.COSMO_PW +'@cloudp1db.mongocluster.cosmos.azure.com/blogdb?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000');
+mongoose.connect('mongodb+srv://nnhieu22:' + process.env.COSMO_PW + '@cloudp1db.mongocluster.cosmos.azure.com/blogdb?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000');
 
 app.use(morgan('dev'));
-app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
 app.set("view engine", "ejs");
+
 
 app.use(express.static(path.join(ROOT_DIR, 'views')));
 
